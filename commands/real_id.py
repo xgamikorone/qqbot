8aa57a630f5bea41b7c825834427485db4f316bd
hanyuu1 <13738325103@126.com>
2026-05-22 15:47:41 +0800

    Update fake id

--- commands/real_id.py
+++ commands/real_id.py
@@ -24,3 +24,27 @@ class RealId(Command):
         avatar = user["user"]["avatar"]
 
         await self.send_reply(message, f"{user_name}")
+
+
+@command("皮套")
+class FakeId(Command):
+    name = "fake_id"
+    cn_name = "皮套"
+
+    async def execute(self, message: Message, args: List[str]):
+        mentions = message.mentions
+        filtered_users = [u for u in mentions if not u.bot]
+        user_id = filtered_users[0].id
+        user = await self.client.api.get_guild_member(message.guild_id, user_id)
+
+        name = user["nick"]
+        avatar = user["user"]["avatar"]
+
+        await self.client.api.post_message(
+            message.channel_id,
+            name,
+            image=avatar,
+            msg_id=message.id
+        )
+
+
