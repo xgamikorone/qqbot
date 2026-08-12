9019e710fa548026e3c95bf5dab389d4db9151c4
hanyuu1 <13738325103@126.com>
2026-08-12 18:36:26 +0800

    feat: add mute command to command imports

--- commands/__init__.py
+++ commands/__init__.py
@@ -31,7 +31,8 @@ from . import (
     real_id,
     owner,
     poll,
-    recall
+    recall,
+    mute
 )
 
 __all__ = ["command", "Command", "_command_registry", "CommandManager", "_command_name_to_formal_name", "_command_alias_to_name"]
